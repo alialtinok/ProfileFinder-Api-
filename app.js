dc2833dedfef59adfa3e1da9107960fea935a409
Ali Altınok <info.alialtinok@gmail.com>
2021-12-09 22:48:09 +0300

    Displaying Data

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const profile = new Profile()
+const ui = new UI();
 const searchProfile = document.querySelector('#searchProfile');
 
 searchProfile.addEventListener('keyup', (event)=> {
+    ui.clear()
     let text = event.target.value
     if(text !== '') {
         profile.getProfile(text).then(res=> {
             if(res.profile.length === 0) {
-
+                ui.showAlert(text)
             }
             else {
-                console.log(res.profile[0])
+                ui.showProfile(res.profile[0])
             }
         })
     } 
